50fa8fb755b3098abcd91e142f3c1d011033b015
Bereket48 <abatebereket536@gmail.com>
2025-06-30 23:44:26 +0200

    prepare to deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 // vite.config.js
-import { defineConfig } from "vite";
+// import { defineConfig } from "vite";
 
+// export default defineConfig({
+//   base: "/netflicx-clone/",
+// });
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 export default defineConfig({
-  base: "/netflix-clone/",
+  plugins: [react()],
+  base: "./",
 });
